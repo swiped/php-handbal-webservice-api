a0fcae650f351af33fb940729fecbf101352e831
Barry vd. Heuvel <barryvdh@gmail.com>
2017-07-29 16:23:53 +0200

    Bump guzzle

--- src/HttpClient/HttpClient.php
+++ src/HttpClient/HttpClient.php
@@ -6,6 +6,9 @@ use GuzzleHttp\ClientInterface as GuzzleClientInterface;
 
 class HttpClient implements HttpClientInterface
 {
+    /** @var GuzzleClient $client */
+    protected $client;
+
     /** @var string $code */
     protected $code;
 
@@ -15,29 +18,20 @@ class HttpClient implements HttpClientInterface
      * @var array
      */
     protected $options = array(
-        'base_url' => 'http://www.knkv.nl/kcp/',
-        'defaults' => [
-            'auth'      => 'knvb',
-            'timeout'   => 30,
-            'headers'   => [
-                'User-Agent' => 'php-knvb-dataservice-api',
-            ],
-        ],
+        'timeout'   => 30,
     );
 
     /**
-     * Construct a new HttpClient instance. Optional parameters can be supplied.
-     * A Guzzle client can optionally be passed as argument, but a new instance
-     * will be created by default.
+     * Construct a new HttpClient instance.
+     * A Guzzle client can optionally be passed as argument,
+     * but a new instance will be created by default.
      *
      * @param string $code
-     * @param array $options
      * @param GuzzleClientInterface $client
      */
-    public function __construct($code, $options = [], GuzzleClientInterface $client = null)
+    public function __construct($code, GuzzleClientInterface $client = null)
     {
         $this->code = $code;
-        $this->options = array_merge($this->options, $options);
         $client = $client ?: new GuzzleClient($this->options);
         $this->client = $client;
     }
@@ -47,9 +41,11 @@ class HttpClient implements HttpClientInterface
      */
     public function post($params = [])
     {
-        return $this->client->post($this->getPath(), [
+        $response = $this->client->request('POST', 'http://www.knkv.nl/kcp/' . $this->getPath(), [
             'body' => $params,
-        ])->json();
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
     }
 
     protected function getPath()
